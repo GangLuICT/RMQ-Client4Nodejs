b368264b3f355d4960e6f22f0ae273c201f00983
ttnn <limingen@aliyun.com>
2016-06-28 21:29:35 +0800

    buffer.toJSON().map error
    not buffer.toJSON().map(.....), but buffer.toJSON().data.map(......)

--- MQMessage.js
+++ MQMessage.js
@@ -29,7 +29,8 @@ var MQMessage = function(topic, tags, keys, body){
     this.keys = keys;
     this.body = body;
     var buffer = iconv.encode(this.body, settings.MsgBodyEncoding);	//string to encoded Buffer
-    var byteArray = java.newArray("byte", buffer.toJSON().map(function(c) { return java.newByte(c); }));	//从Buffer生成byte array的固定方式！ 要先转换为Byte数组，也就是toJSON
+    //not buffer.toJSON().map(.....), but buffer.toJSON().data.map(......)
+    var byteArray = java.newArray("byte", buffer.toJSON().data.map(function(c) { return java.newByte(c); }));	//从Buffer生成byte array的固定方式！ 要先转换为Byte数组，也就是toJSON
     //var byteArray = java.newArray("byte", this.body.split('').map(function(c) { return java.newByte(String.prototype.charCodeAt(c)); }));	//从string生成byte array的固定方式！
     //sync methods:
     this.msg = new Message(this.topic, this.tags, this.keys, byteArray); //string to bytes
