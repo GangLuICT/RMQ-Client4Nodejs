7b2781c1178f007952120201dcc92276ad0aeab6
Gang Lu <gang.lu.ict@gmail.com>
2016-07-13 18:00:18 +0800

    添加异步PullConsumer,循环执行

--- MQPullConsumer.js
+++ MQPullConsumer.js
@@ -10,6 +10,8 @@ var settings = require("./settings_MQ");   //配置信息
 var logger = settings.logger;
 var moment = require('moment'); //时间
 
+var PullStatus = MQM.PullStatus;
+
 var java = require("java");
 
 var DefaultMQPullConsumer= java.import('com.alibaba.rocketmq.client.consumer.DefaultMQPullConsumer');
@@ -73,6 +75,16 @@ MQPullConsumer.prototype.fetchSubscribeMessageQueues = function (topic, callback
     callback && callback();
 };
 
+MQPullConsumer.prototype.setBrokerSuspendMaxTimeMillis = function (brokerSuspendMaxTimeMillis, callback) {
+    this.consumer.setBrokerSuspendMaxTimeMillisSync(brokerSuspendMaxTimeMillis);
+    callback && callback();
+};
+
+MQPullConsumer.prototype.setConsumerTimeoutMillisWhenSuspend = function (consumerTimeoutMillisWhenSuspend, callback) {
+    this.consumer.setConsumerTimeoutMillisWhenSuspendSync(consumerTimeoutMillisWhenSuspend);
+    callback && callback();
+};
+
 //获取某个MQ中的当前消息的offset
 MQPullConsumer.prototype.getMessageQueueOffset = function (mq) {
     var haskey = this.offseTable[mq.getQueueIdSync()];
@@ -86,5 +98,65 @@ MQPullConsumer.prototype.getMessageQueueOffset = function (mq) {
 MQPullConsumer.prototype.putMessageQueueOffset = function (mq, offset) {
     this.offseTable[mq.getQueueIdSync()] = offset;
 };
+
+MQPullConsumer.prototype.setPullHandler = function (topic, tags, consumeMessage){
+    this.topic = topic;
+    this.tags = tags;
+    this.consumeMessage = consumeMessage;
+};
+
+//开始循环拉取消息
+MQPullConsumer.prototype.pullLoop = function(){
+    var self = this;
+    //获取所有消息队列,返回值存储到consumer.mqs
+    self.fetchSubscribeMessageQueues(this.topic, function(){
+        //TODO:
+        //    1. fetchSubscribeMessageQueues可能返回异常：Can not find Message Queue for this topic
+        //       如果不存在Topic，则创建topic：createTopic(String key, String newTopic, int queueNum)
+        //    2. MessageQueue的数目，如何确定的！
+        //    3. PullConsumer没有ConsumeFromWhere这项设置
+        logger.debug('After fetch subscribe messge queues: ' + self.mqs.length);
+        for (var mqid in self.mqs) {
+            //consumer.mqs.forEach(function(mq){
+            var mq = self.mqs[mqid];
+            logger.debug("Pulling from message queue: " + mq.getQueueIdSync());
+            try {
+                pullMessagesAsync(self, mq);
+            } catch (ex) {
+                if (ex.cause)	// Exception from node-java
+                    logger.error(ex.cause.getMessageSync());
+                else
+                    logger.error(ex.name + ': ' + ex.message);
+            }
+        }
+    });
+};
+
+function pullMessagesAsync(consumer, mq) {
+    //var pullResult = self.pullBlockIfNotFound(mq, '', self.getMessageQueueOffset(mq), settings.pullMaxNums);
+    consumer.pullBlockIfNotFoundAsync(mq, this.topic, consumer.getMessageQueueOffset(mq), settings.pullMaxNums, function (pullResult) {
+        pullMessagesAsync(consumer, mq);    //继续异步调用、拉取消息
+        if (pullResult) {
+            consumer.putMessageQueueOffset(mq, pullResult.getNextBeginOffsetSync());
+            var pullStatus = PullStatus[pullResult.getPullStatusSync().toString()];	// JAVA中的enum对应到Python中没有转换为Int，enum对象转换为string的时候是其枚举值的名字，而不是enum的值（0,1...）！
+            if (pullStatus == PullStatus['FOUND']) {
+                logger.debug('Found');
+                logger.debug(pullResult.toString());
+                var msgList = pullResult.getMsgFoundListSync();
+                this.consumeMessage(msgList);
+            } else if (pullStatus == PullStatus['NO_NEW_MSG']) {
+                logger.debug('NO_NEW_MSG');
+            } else if (pullStatus == PullStatus['NO_MATCHED_MSG']) {
+                logger.debug('NO_MATCHED_MSG');
+            } else if (pullStatus == PullStatus['OFFSET_ILLEGAL']) {
+                logger.debug('OFFSET_ILLEGAL');
+            } else {
+                logger.error('Wrong pull status: ' + pullStatus.toString());
+            }
+        } else {
+            logger.debug('This pulling does not get any result!');
+        }
+    });
+}
 
 module.exports = MQPullConsumer;
---
+++ test/PullConsumer2.js
@@ -0,0 +1,82 @@
+/**
+ * Gang Lu created at 2016-6-13
+ * E-mail: gang.lu.ict@gmail.com
+ *
+ * Copyright (c) 2016 bafst.com, All rights reserved.
+ */
+
+"use strict";
+
+var iconv = require('iconv-lite');
+
+var settings = require("../settings_MQ");
+var logger = settings.logger;
+
+var java = require("java");
+
+// Configure JVM
+java.classpath.push(settings.JAVA_EXT_DIRS);
+settings.JVM_OPTIONS.forEach(function(opt){
+    java.options.push(opt);
+});
+
+// Import MQPullConsumer
+var MQM = require("../MQMessage");
+//var MQMessage = MQM.MQMessage;
+var PullStatus = MQM.PullStatus;
+
+var MQPullConsumer = require("../MQPullConsumer");
+
+var consumer = new MQPullConsumer('MQClient4Python-Consumer', 'jfxr-7:9876;jfxr-6:9876');
+consumer.init();
+consumer.start();
+
+consumer.setPullHandler("FANSHOP-BENCH", "CNotify_IOS_2", pullHandler.consumeMessage1);
+
+consumer.pullLoop();
+
+while(true){
+   setTimeout(function(){
+        consumer.shutdown();
+   }, 1000000);	//睡眠1秒
+}
+
+
+function consumeMessage1(_msgs) {
+    //操作流程:
+    //   直接推送, 无须存数据库操作、无须查找
+    //发送失败、重连操作!
+    //TODO: deviceToken要是失效/错误? 设备端的检查=>避免消息误发
+    var msgs = _msgs.toArraySync();
+    console.log("Calling consumeMessage of MessageListenerConcurrently1");
+    //console.log("11" + ConsumeConcurrentlyStatus.RECONSUME_LATER);
+    //msg = msgs.get(JInt(0))
+    msgs.forEach(function (msg) {
+        var topic = msg.getTopicSync();
+        var tags = msg.getTagsSync();
+        //var bodybytes = new Buffer(msg.getBody());
+        //var body = bodybytes.toString(settings.MsgBodyEncoding);  //byte to string
+        //var body = msg.getBodySync().toString(settings.MsgBodyEncoding);
+        var body = iconv.decode(new Buffer(msg.getBodySync()), settings.MsgBodyEncoding);
+
+        logger.debug(msg.toStringSync());
+        // In Python 2.x, bytes is just an alias for str. 所以bytes解码时要注意了, msg.body.decode会出错(bytes没有decode方法)！
+        //logger.debug("Message body: " + str(msg.getBody()))
+        //logger.debug("Message body: " + str(msg.getBody()).decode(settings.MsgBodyEncoding))
+        logger.debug("Message body: " + body);
+
+        if (topic == consumer.topic && tags == consumer.tags) {
+            // 执行消费逻辑
+            logger.debug("Got message with topic " + topic + " and tags " + tags);
+            //由于APN是异步执行pushNotification,每次都得返回消费成功
+            //只有在pushNotification本身执行失败的情况下才会返回消费失败
+
+        } else {// 错误的Tag
+            logger.error("Error: expecting message with topic " + config.OptConsumer1.Topic + " and tags " + config.OptConsumer1.Tags
+                + ", but got topic " + topic + " and tags " + tags);
+            //return ConsumeConcurrentlyStatus.RECONSUME_LATER;
+        }
+    });
+    //return ConsumeConcurrentlyStatus.CONSUME_SUCCESS;
+}
+
